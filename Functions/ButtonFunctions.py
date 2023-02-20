d4b65001d35763578ad22d0a56e4d0b1df6411c5
Hossein Zalaghi <hossein73.z@gmail.com>
2023-02-20 22:48:53 +0330

    get_pressed_btn completed

--- Functions/ButtonFunctions.py
+++ Functions/ButtonFunctions.py
@@ -1,4 +1,4 @@
-from Functions.Coloring import red, magenta, yellow
+from Functions.Coloring import red, magenta, yellow, bright
 from Objects.Person import Person
 from Objects.Buttton import Button
 from Objects.SPButtton import SPButton
@@ -7,7 +7,7 @@ from Functions.DatabaseCRUD import read, SP_BUTTONS_TABLE, BUTTONS_TABLE
 from telegram import KeyboardButton
 
 
-def get_btn_list(person: Person, button_id):
+def get_btn_list(person: Person, button_id: int):
     # Get all the buttons from database
     raw_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=person.admin)
     raw_sp_btns: list[SPButton] = read(SP_BUTTONS_TABLE, SPButton, admin=person.admin)
@@ -30,3 +30,41 @@ def get_btn_list(person: Person, button_id):
 
     else:
         return None
+
+
+def get_pressed_btn(person: Person, text: str):
+    # Get pressed button from database
+    raw_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=person.admin, belong=person.last_button_id, text=text)
+
+    if raw_btns:
+        # Received text was a normal button. Returning
+        return {'button': raw_btns[0], 'is_special': False}
+    else:
+        # Received text was not a normal button
+        pressed_btn = None
+
+        # Get last button from database
+        last_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=person.admin, id=person.last_button_id)
+        if last_btns:
+            sp_btn_ids = []
+            try:
+                for js in last_btns[0].sp_btns:
+                    for j in js:
+                        sp_btn_ids.append(j)
+
+                if sp_btn_ids:
+                    raw_sp_btns = read(SP_BUTTONS_TABLE, SPButton, admin=person.admin)
+                    for sp_btn_id in sp_btn_ids:
+                        for raw_sp_btn in raw_sp_btns:
+                            if raw_sp_btn.text == text and raw_sp_btn.id == sp_btn_id:
+                                pressed_btn = {'button': raw_sp_btn, 'is_special': True}
+                                break
+
+                    return pressed_btn
+            except TypeError as e:
+                print("get_pressed_btn: " + red(e))
+
+        else:
+            # Major Error!!
+            print('get_pressed_btn: ' + red(bright('User "btn_id" matches to no stored buttons')))
+            return None
