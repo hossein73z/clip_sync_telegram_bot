ab320077b1f53ef324a5852184f271542feabf47
Hossein Zalaghi <hossein73.z@gmail.com>
2023-02-23 14:36:25 +0330

    Some cleaning

--- Functions/ButtonFunctions.py
+++ Functions/ButtonFunctions.py
@@ -5,11 +5,18 @@ from MyObjects import Person, Button, SPButton
 
 
 def get_btn_list(person: Person, button_id: int):
-    # Get all the buttons from database
+    """
+    Get all the buttons from database
+
+    :param person: object of Person for last pressed button reference and admin privileges.
+    :param button_id: Id of the button to get its subset buttons
+    :return: array of array of KeyboardButton object
+    """
+
     raw_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=person.admin)
     raw_sp_btns: list[SPButton] = read(SP_BUTTONS_TABLE, SPButton, admin=person.admin)
 
-    # Create dictionary with buttons and their ids
+    # Create dictionaries with buttons and their ids
     buttons_dict: {int, Button} = {b.id: b for b in raw_btns} if raw_btns else None
     sp_buttons_dict: {int, SPButton} = {b.id: b for b in raw_sp_btns} if raw_sp_btns else None
 
@@ -29,13 +36,19 @@ def get_btn_list(person: Person, button_id: int):
         return None
 
 
-def get_pressed_btn(person: Person, text: str):
-    # Get pressed button from database
+def get_pressed_btn(person: Person, text: str) -> dict | None:
+    """
+    find and return pressed button from database based on the received text and last pressed button
+
+    :param person: object of Person for last pressed button reference and admin privileges.
+    :param text: text of the wanted button.
+    :return: dict:{'button': <Pressed_button>, 'is_special': <bool>} if the text matches a button and None otherwise.
+    """
     raw_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=person.admin, belong=person.btn_id, text=text)
 
     if raw_btns:  # Received text was a normal button. Returning
-        return {'button': raw_btns[0], 'is_special': False}
 
+        return {'button': raw_btns[0], 'is_special': False}
     else:  # Received text was not a normal button
 
         last_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=person.admin, id=person.btn_id)
@@ -44,5 +57,11 @@ def get_pressed_btn(person: Person, text: str):
         if ids:
             # Find pressed button using text and id list
             sp_btns = read(SP_BUTTONS_TABLE, SPButton, admin=person.admin, id=set(ids), text=text)
-            if sp_btns:
+            if sp_btns:  # Match found. Returning the value
                 return {'button': sp_btns[0], 'is_special': True}
+
+            else:  # No button matched id-text combination
+                return None
+
+        else:  # The last button have no special button
+            return None
