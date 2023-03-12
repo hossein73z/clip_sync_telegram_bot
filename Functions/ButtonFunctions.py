9a362e1074aa580100c2251172a7d3df7bdbb388
Hossein Zalaghi <hossein73.z@gmail.com>
2023-03-12 19:33:42 +0330

    implimented SQLAlchemy

--- Functions/ButtonFunctions.py
+++ Functions/ButtonFunctions.py
@@ -1,20 +1,21 @@
 from telegram import KeyboardButton
 
-from Functions.DatabaseCRUD import BUTTONS_TABLE, SP_BUTTONS_TABLE, read
+from Functions.DatabaseCRUD import read
 from MyObjects import Person, Button, SPButton
 
 
-def get_btn_list(person: Person, button_id: int):
+def get_btn_list(person_btn_id: int, admin: int, button_id: int):
     """
     Get all the buttons from database
 
-    :param person: object of Person for admin privileges.
+    :param person_btn_id:ID of person's last pressed button
+    :param admin: Person administration level
     :param button_id: ID of the button to get its subset buttons
     :return: array of array of KeyboardButton object or None if there's no subset buttons
     """
 
-    raw_btns: list[Button] = read(BUTTONS_TABLE, Button, admin=set(range(0, person.admin + 1)))
-    raw_sp_btns: list[SPButton] = read(SP_BUTTONS_TABLE, SPButton, admin=set(range(0, person.admin + 1)))
+    raw_btns: list[Button] = read(Button, admin=set(range(0, admin + 1)))
+    raw_sp_btns: list[SPButton] = read(SPButton, admin=set(range(0, admin + 1)))
 
     # Create dictionaries with buttons and their ids
     buttons_dict: {int, Button} = {b.id: b for b in raw_btns} if raw_btns else None
@@ -36,17 +37,18 @@ def get_btn_list(person: Person, button_id: int):
         return None
 
 
-def get_pressed_btn(person: Person, text: str) -> dict | None:
+def get_pressed_btn(person_btn_id: int, admin: int, text: str) -> dict | None:
     """
     find and return pressed button from database based on the received text and last pressed button
 
-    :param person: object of Person for last pressed button reference and admin privileges.
+    :param person_btn_id:ID of person's last pressed button
+    :param admin: Person administration level
     :param text: text of the wanted button.
     :return: dict:{'button': <Pressed_button>, 'is_special': <bool>} if the text matches a button and None otherwise.
     """
-    raw_btns: list[Button] = read(BUTTONS_TABLE, Button,
-                                  admin=set(range(0, person.admin + 1)),
-                                  belong=person.btn_id,
+    raw_btns: list[Button] = read(Button,
+                                  admin=set(range(0, admin + 1)),
+                                  belong=person_btn_id,
                                   text=text)
 
     if raw_btns:  # Received text was a normal button. Returning
@@ -54,9 +56,9 @@ def get_pressed_btn(person: Person, text: str) -> dict | None:
         return {'button': raw_btns[0], 'is_special': False}
     else:  # Received text was not a normal button
 
-        last_btns: list[Button] = read(BUTTONS_TABLE, Button,
-                                       admin=set(range(0, person.admin + 1)),
-                                       id=person.btn_id)
+        last_btns: list[Button] = read(Button,
+                                       admin=set(range(0, admin + 1)),
+                                       id=person_btn_id)
         if last_btns[0].sp_btns:
             ids = [item for items in last_btns[0].sp_btns for item in items]  # Create a list of ids from nested list
         else:
@@ -64,7 +66,7 @@ def get_pressed_btn(person: Person, text: str) -> dict | None:
 
         if ids:
             # Find pressed button using text and id list
-            sp_btns = read(SP_BUTTONS_TABLE, SPButton, admin=set(range(0, person.admin + 1)), id=set(ids), text=text)
+            sp_btns = read(SPButton, admin=set(range(0, admin + 1)), id=set(ids), text=text)
             if sp_btns:  # Match found. Returning the value
                 return {'button': sp_btns[0], 'is_special': True}
 
